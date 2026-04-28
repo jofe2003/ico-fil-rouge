86d8eaeebc2838e78602904d0b5c6dba038a5117
jofe2003 <jo.fe2003@usp.br>
2026-04-28 11:11:07 +0200

    removing database

--- BaseDeDonnees/BaseExcel/extract.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df = pd.read_excel(r"C:\Users\natyj\Documents\ICO\Program\ico-fil-rouge\CommonFiles\6_detail_table_cust_depots_distances.xls")
-df.to_json("6_detail_table_cust_depots_distances.json", orient="records", indent=4)
